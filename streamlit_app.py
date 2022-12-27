a5bd71febfbe38f47ea71a9b8cb2466dd48825bf
omid-flipside <121507561+omid-flipside@users.noreply.github.com>
2022-12-27 21:30:12 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,36 +21,8 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.header('st.write')
-
-# Example 1
-
-st.write('Hello, *World!* :sunglasses:')
-
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
-
-# Example 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+st.caption('This is a string that explains something above.')
+st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
 #with st.echo(code_location='below'):
     #total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     #num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
