ca3fc2b94f01de18972a5fec747f918361a2bbd6
omid-flipside <121507561+omid-flipside@users.noreply.github.com>
2022-12-27 20:19:29 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,18 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
+#"""
 # Welcome to Streamlit!
 
- Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+ #Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+#forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+#In the meantime, below is an example of what you can do with just a few lines of code:
 
-"""
-st.write('hello')
+#"""
+#st.write('hello')
 st.header('st.button')
 if st.button('say hello'):st.write('why hello')
  else st.write('goodbby')
