48d1ed194fbcc6ab350b58f3cb3838ebc9b7e780
omid-flipside <121507561+omid-flipside@users.noreply.github.com>
2022-12-27 20:16:21 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 """
 st.write('hello')
+st.header('st.button')
+if st.button('say hello'):st.write('why hello')
+ else st.write('goodbby')
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
