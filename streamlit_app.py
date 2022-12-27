31ba206b909da35ffd8426421def63cc1fc5bffa
omid-flipside <121507561+omid-flipside@users.noreply.github.com>
2022-12-27 21:31:22 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ import streamlit as st
 
 st.caption('This is a string that explains something above.')
 st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
+st.image
 #with st.echo(code_location='below'):
     #total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     #num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
