8f0d8b7086be79d9159c56cddef3c06ca5cee2b0
omid-flipside <121507561+omid-flipside@users.noreply.github.com>
2022-12-27 20:24:26 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+#from collections import namedtuple
+#import altair as alt
+#import math
+#import pandas as pd
 import streamlit as st
 
 #"""
