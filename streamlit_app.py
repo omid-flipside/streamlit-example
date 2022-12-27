66cc18564e8ae864c3700ba56fa693e3ca759def
omid-flipside <121507561+omid-flipside@users.noreply.github.com>
2022-12-27 20:48:48 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,41 @@
 
 #"""
 #st.write('hello')
+import numpy as np
+import altair as alt
+import pandas as pd
 import streamlit as st
-st.write('blockchain')
-st.header('chain')
 
-if st.button('Say hello'):
-     st.write('hi')
-else:
-     st.write('Goodbye')
+st.header('st.write')
 
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
 #with st.echo(code_location='below'):
     #total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     #num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
